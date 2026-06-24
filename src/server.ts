5482af4b5891d29bd2b6ec06f2ea283fbc74a47b
Shashuvo <shahariat2810@gmail.com>
2026-06-24 13:12:29 +0600

    prisma connection given without any models

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,19 @@
 import app from "./app";
+import "dotenv/config";
+import { prisma } from "./lib/prisma";
 
 const PORT = process.env.PORT || 5000;
 
 async function main() {
     try {
-        app.listen(PORT,()=>{
+        await prisma.$connect();
+        console.log("connected to DB successfully!");
+        app.listen(PORT, () => {
             console.log(`server is running on port ${PORT}`);
         })
     } catch (error) {
         console.log("Error starting server:", error);
+        await prisma.$disconnect();
         process.exit(1);
     }
 };
