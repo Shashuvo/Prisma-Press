c4fb7fb0f693b3468bced4c20d29c6a8e91b2a71
Shashuvo <shahariat2810@gmail.com>
2026-06-27 20:33:47 +0600

    cookies set while log in

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -6,13 +6,25 @@ import { sendResponse } from "../../utils/sendResponse";
 
 const loginUser = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const payload = req.body;
-    const loginResult = await authService.loginUser(payload);
+    const { accessToken, refreshToken } = await authService.loginUser(payload);
+    res.cookie("accessToken", accessToken, {
+        httpOnly: true,
+        secure: false,
+        sameSite: "none",
+        maxAge: 1 * 24 * 60 * 60 * 1000
+    });
+    res.cookie("refreshToken", refreshToken, {
+        httpOnly: true,
+        secure: false,
+        sameSite: "none",
+        maxAge: 7 * 24 * 60 * 60 * 1000
+    });
     sendResponse(res, {
         success: true,
         statusCode: httpStatus.OK,
         message: "User logged in successfully",
         data: {
-            loginResult
+            accessToken, refreshToken
         }
     });
 });
