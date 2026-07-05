9d0b0af7d2f338a77e99a2b5d7aae95126cdcceb
Shashuvo <shahariat2810@gmail.com>
2026-07-05 20:07:31 +0600

    comments apis implemented

--- src/modules/comment/comment.controller.ts
+++ src/modules/comment/comment.controller.ts
@@ -1,28 +1,85 @@
 import { NextFunction, Request, Response } from "express";
 import { catchAsync } from "../../utils/catchAsync"
+import { commentService } from "./comment.service";
+import { sendResponse } from "../../utils/sendResponse";
+import httpStatus from "http-status";
 
+// get authors comment
 const getAuthorCommentsById = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-
+    const { authorId } = req.params
+    const result = await commentService.getAuthorCommentsByIdFromDB(authorId as string)
+    sendResponse(res, {
+        success: true,
+        statusCode: httpStatus.OK,
+        message: "Comments retrieved successfully",
+        data: result
+    })
 });
 
+// get comment by post id
 const getCommentById = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-
+    const { postId } = req.params
+    const result = await commentService.getCommentByIdFromDB(postId as string)
+    sendResponse(res, {
+        success: true,
+        statusCode: httpStatus.OK,
+        message: "Comment retrieved successfully",
+        data: result
+    })
 });
 
+// create comment
 const createComment = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-
+    const authorId = req.user?.id as string;
+    const result = await commentService.createCommentIntoDB(authorId, req.body);
+    sendResponse(res, {
+        success: true,
+        statusCode: httpStatus.CREATED,
+        message: "Comment created successfully",
+        data: result
+    })
 });
 
+// update comment by id
 const updateCommentById = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-
+    const user = req.user;
+    const { commentId } = req.params;
+    const authorId = user?.id as string;
+    const payload = req.body;
+    const result = await commentService.updateCommentByIdIntoDB(commentId as string, payload, authorId)
+    sendResponse(res, {
+        success: true,
+        statusCode: httpStatus.OK,
+        message: "Comment updated successfully",
+        data: result
+    })
 });
 
+// delete comment by id
 const deleteCommentById = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-
+    const user = req.user;
+    const { commentId } = req.params;
+    const authorId = user?.id as string;
+    const result = await commentService.deleteCommentByIdFromDB(commentId as string, authorId)
+    sendResponse(res, {
+        success: true,
+        statusCode: httpStatus.OK,
+        message: "Comment deleted successfully",
+        data: result
+    })
 });
 
+// moderate comment by id
 const moderateCommentById = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-
+    const { commentId } = req.params;
+    const payload = req.body;
+    const result = await commentService.moderateCommentByIdIntoDB(commentId as string, payload);
+    sendResponse(res, {
+        success: true,
+        statusCode: httpStatus.OK,
+        message: "Comment moderated successfully",
+        data: result
+    });
 });
 
 export const commentController = {
--- src/modules/comment/comment.service.ts
+++ src/modules/comment/comment.service.ts
@@ -1,25 +1,132 @@
-const getAuthorCommentsByIdFromDB = async () => {
+import { prisma } from "../../lib/prisma"
+import { ICreateCommentPayload, IModerateCommentPayload, IUpdateCommentPayload } from "./comment.interface"
 
+// get comment by author id
+const getAuthorCommentsByIdFromDB = async (authorId: string) => {
+    const comments = await prisma.comment.findMany({
+        where: {
+            authorId
+        },
+        orderBy: { createdAt: "desc" },
+        include: {
+            post: {
+                select: {
+                    id: true,
+                    title: true
+                }
+            }
+        }
+    })
+    return comments;
 }
 
-const getCommentByIdFromDB = async () => {
-
+// get comment by post id
+const getCommentByIdFromDB = async (postId: string) => {
+    const comment = await prisma.comment.findMany({
+        where: {
+            postId
+        }
+    })
+    return comment;
 }
 
-const createCommentIntoDB = async () => {
+// create comment
+const createCommentIntoDB = async (authorId: string, payload: ICreateCommentPayload) => {
+    await prisma.post.findUniqueOrThrow({
+        where: {
+            id: payload.postId
+        }
+    })
 
+    const comment = await prisma.comment.create({
+        data: {
+            ...payload,
+            authorId
+        },
+        // include:{
+        //     post : true
+        // }
+    })
+
+    return comment;
 }
 
-const updateCommentByIdIntoDB = async () => {
+// update comment by id
+const updateCommentByIdIntoDB = async (commentId: string, data: IUpdateCommentPayload, authorId: string) => {
+    const commentData = await prisma.comment.findUniqueOrThrow({
+        where: {
+            id: commentId,
+            authorId
+        },
+        select: {
+            id: true
+        }
+    })
 
+    // if (!commentData) {
+    //     throw new Error("Your provided input is invalid!")
+    // }
+
+    const comment = await prisma.comment.update({
+        where: {
+            id: commentId,
+            authorId
+        },
+        data
+    })
+
+    return comment;
 }
 
-const deleteCommentByIdFromDB = async () => {
+// delete comment by id
+const deleteCommentByIdFromDB = async (commentId: string, authorId: string) => {
+    const commentData = await prisma.comment.findUniqueOrThrow({
+        where: {
+            id: commentId,
+            authorId
+        },
+        select: {
+            id: true
+        }
+    })
 
+    // if (!commentData) {
+    //     throw new Error("Your provided input is invalid!")
+    // }
+
+    const comment = await prisma.comment.delete({
+        where: {
+            id: commentData.id
+        }
+    });
+
+    return null;
 }
 
-const moderateCommentByIdIntoDB = async () => {
+// moderate comment by id
+const moderateCommentByIdIntoDB = async (id: string, data: IModerateCommentPayload) => {
+    const commentData = await prisma.comment.findUniqueOrThrow({
+        where: {
+            id
+        },
+        select: {
+            id: true,
+            status: true
+        }
+    });
 
+    if (commentData.status === data.status) {
+        throw new Error(`Your provided status (${data.status}) is already up to date.`)
+    }
+
+    const comment = await prisma.comment.update({
+        where: {
+            id
+        },
+        data
+    });
+
+    return comment;
 }
 
 export const commentService = {
