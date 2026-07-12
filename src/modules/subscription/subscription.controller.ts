b6d7cd2c6d6c5b9854c40c2fb1d8969e54f573bf
Shashuvo <shahariat2810@gmail.com>
2026-07-12 22:04:27 +0600

    subscription checkout route added

---
+++ src/modules/subscription/subscription.controller.ts
@@ -0,0 +1,21 @@
+import httpStatus from 'http-status';
+import { NextFunction, Request, Response } from "express";
+import { catchAsync } from "../../utils/catchAsync";
+import { subscriptionService } from "./subscription.service";
+import { sendResponse } from "../../utils/sendResponse";
+
+// create check out after payment
+const createCheckOutSession = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const userId = req.user?.id as string;
+    const result = await subscriptionService.createCheckoutSession(userId);
+    sendResponse(res, {
+        success: true,
+        statusCode: httpStatus.CREATED,
+        message: "Checkout completed successfully.",
+        data: result
+    })
+});
+
+export const subscriptionController = {
+    createCheckOutSession
+}
--- src/modules/subscription/subscription.route.ts
+++ src/modules/subscription/subscription.route.ts
@@ -1,5 +1,10 @@
 import { Router } from "express";
+import { auth } from "../../middleware/auth";
+import { subscriptionController } from "./subscription.controller";
 
 const router = Router();
+
+// create check out after payment
+router.post("/checkout", auth(), subscriptionController.createCheckOutSession);
 
 export const subscriptionRoutes = router;
